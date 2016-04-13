be0c5d24de835abb2a8d9cbef00b46649005830d
shuzhan <shuzhan@163.com>
2016-04-13 21:09:57 +0800

    添加了一部分操作员js

--- AngularJS/controller/configure_controller.js
+++ AngularJS/controller/configure_controller.js
@@ -6,12 +6,21 @@
 
 App.controller('ConfigureController',['$scope','ConfigureService',function($scope,ConfigureService){
     var self = this;
-    self.users = [];
+    self.users = [];//操作员表
+    self.rooms = [];//房间定义
+    self.roomCategories = [];//房间类别
+    self.protocols = [];//协议房价
+    self.protocolMaps = [];//协议映射----协议房价从表
+    self.otherParams = [];//其他参数
+    self.guestSources = [];//客源
+    self.currencies = [];//币种
+    self.companyCards = [];//单位帐卡
+    self.companyLords = [];//单位签单人----单位帐卡从表 3列
     /**
-     * 获取全部操作员信息
+     * 操作员
      */
-    self.getAllUser = function () {
-        ConfigureService.getAllUser()
+    self.getAllUsers = function () {
+        ConfigureService.getAllUsers()
             .then(
             function (d) {
                 self.users = d;
@@ -21,4 +30,143 @@ App.controller('ConfigureController',['$scope','ConfigureService',function($scop
             }
         );
     };
-}])
+    /**
+     * 房间定义
+     */
+    self.getAllRooms = function () {
+        ConfigureService.getAllRooms()
+            .then(
+            function (d) {
+                self.rooms = d;
+            },
+            function (errResponse) {
+                console.error('Error while getting rooms:' + errResponse.toString());
+            }
+        );
+    };
+    /**
+     * 房间类别
+     */
+    self.getAllRoomCategories = function () {
+        ConfigureService.getAllRoomCategories()
+            .then(
+            function (d) {
+                self.roomCategories = d;
+            },
+            function (errResponse) {
+                console.error('Error while getting roomCategories:' + errResponse.toString());
+            }
+        );
+    };
+    /**
+     * 协议房价
+     */
+    self.getAllProtocols = function () {
+        ConfigureService.getAllProtocols()
+            .then(
+            function (d) {
+                self.protocols = d;
+            },
+            function (errResponse) {
+                console.error('Error while getting protocols:' + errResponse.toString());
+            }
+        );
+    };
+    /**
+     * 协议映射
+     */
+    self.getAllProtocolMaps = function () {
+        ConfigureService.getAllProtocolMaps()
+            .then(
+            function (d) {
+                self.protocolMaps = d;
+            },
+            function (errResponse) {
+                console.error('Error while getting protocolMaps:' + errResponse.toString());
+            }
+        );
+    };
+    /**
+     * 其他参数
+     */
+    self.getAllOtherParams = function () {
+        ConfigureService.getAllOtherParams()
+            .then(
+            function (d) {
+                self.otherParams = d;
+            },
+            function (errResponse) {
+                console.error('Error while getting otherParams:' + errResponse.toString());
+            }
+        );
+    };
+    /**
+     * 客源
+     */
+    self.getAllGuestSources = function () {
+        ConfigureService.getAllGuestSources()
+            .then(
+            function (d) {
+                self.guestSources = d;
+            },
+            function (errResponse) {
+                console.error('Error while getting guestSources:' + errResponse.toString());
+            }
+        );
+    };
+    /**
+     * 币种
+     */
+    self.getAllCurrencies = function () {
+        ConfigureService.getAllCurrencies()
+            .then(
+            function (d) {
+                self.currencies = d;
+            },
+            function (errResponse) {
+                console.error('Error while getting currencies:' + errResponse.toString());
+            }
+        );
+    };
+    /**
+     * 单位帐卡
+     */
+    self.getAllCompanyCards = function () {
+        ConfigureService.getAllCompanyCards()
+            .then(
+            function (d) {
+                self.companyCards = d;
+            },
+            function (errResponse) {
+                console.error('Error while getting companyCards:' + errResponse.toString());
+            }
+        );
+    };
+    /**
+     * 单位签单人
+     */
+    self.getAllCompanyLords = function () {
+        ConfigureService.getAllCompanyLords()
+            .then(
+            function (d) {
+                self.companyLords = d;
+            },
+            function (errResponse) {
+                console.error('Error while getting companyLords:' + errResponse.toString());
+            }
+        );
+    };
+
+
+    /*页面加载之后调用的方法*/
+    self.getAllUsers();
+    self.getAllRooms();
+    self.getAllRoomCategories();
+    self.getAllProtocols();
+    self.getAllProtocolMaps();
+    self.getAllOtherParams();
+    self.getAllGuestSources();
+    self.getAllCurrencies();
+    self.getAllCompanyCards();
+    self.getAllCompanyLords();
+}]);
--- AngularJS/service/configure_service.js
+++ AngularJS/service/configure_service.js
@@ -7,17 +7,156 @@
 
 App.factory('ConfigureService', ['$http', '$q', 'host', function ($http, $q, host) {
     return {
-        getAllUser:function(){
+        /**
+         * 操作员
+         */
+        getAllUsers:function(){
             return $http.get(host+'/user')
                 .then(
                 function (response) {
                     return response.data;
                 },
                 function (errResponse) {
-                    console.error('Error while fetching all');
+                    console.error('Error while fetching all user');
+                    return $q.reject(errResponse);
+                }
+            );
+        },
+        /**
+         * 房间定义
+         */
+        getAllRooms:function(){
+            return $http.get(host+'/room')
+                .then(
+                function (response) {
+                    return response.data;
+                },
+                function (errResponse) {
+                    console.error('Error while fetching all rooms');
+                    return $q.reject(errResponse);
+                }
+            );
+        },
+        /**
+         * 房间类别
+         */
+        getAllRoomCategories:function(){
+            return $http.get(host+'/roomCategory')
+                .then(
+                function (response) {
+                    return response.data;
+                },
+                function (errResponse) {
+                    console.error('Error while fetching all roomCategories');
+                    return $q.reject(errResponse);
+                }
+            );
+        },
+        /**
+         * 协议房价
+         */
+        getAllProtocols:function(){
+            return $http.get(host+'/protocol')
+                .then(
+                function (response) {
+                    return response.data;
+                },
+                function (errResponse) {
+                    console.error('Error while fetching all protocols');
+                    return $q.reject(errResponse);
+                }
+            );
+        },
+        /**
+         * 协议映射
+         */
+        getAllProtocolMaps:function(){
+            return $http.get(host+'/protocolMap')
+                .then(
+                function (response) {
+                    return response.data;
+                },
+                function (errResponse) {
+                    console.error('Error while fetching all protocolMaps');
+                    return $q.reject(errResponse);
+                }
+            );
+        },
+        /**
+         * 其他参数
+         */
+        getAllOtherParams:function(){
+            return $http.get(host+'/otherParam')
+                .then(
+                function (response) {
+                    return response.data;
+                },
+                function (errResponse) {
+                    console.error('Error while fetching all otherParams');
+                    return $q.reject(errResponse);
+                }
+            );
+        },
+        /**
+         * 客源
+         */
+        getAllGuestSources:function(){
+            return $http.get(host+'/guestSource')
+                .then(
+                function (response) {
+                    return response.data;
+                },
+                function (errResponse) {
+                    console.error('Error while fetching all guestSources');
+                    return $q.reject(errResponse);
+                }
+            );
+        },
+        /**
+         * 币种
+         */
+        getAllCurrencies:function(){
+            return $http.get(host+'/currency')
+                .then(
+                function (response) {
+                    return response.data;
+                },
+                function (errResponse) {
+                    console.error('Error while fetching all currencies');
+                    return $q.reject(errResponse);
+                }
+            );
+        },
+        /**
+         * 单位帐卡
+         */
+        getAllCompanyCards:function(){
+            return $http.get(host+'/companyCard')
+                .then(
+                function (response) {
+                    return response.data;
+                },
+                function (errResponse) {
+                    console.error('Error while fetching all companyCards');
+                    return $q.reject(errResponse);
+                }
+            );
+        },
+        /**
+         * 单位签单人
+         */
+        getAllCompanyLords:function(){
+            return $http.get(host+'/companyLord')
+                .then(
+                function (response) {
+                    return response.data;
+                },
+                function (errResponse) {
+                    console.error('Error while fetching all companyLords');
                     return $q.reject(errResponse);
                 }
             );
         }
+
     }
-}])
+}]);
