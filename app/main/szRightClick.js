a3ffeaf4800c85a31074c6aac32d004ff1fe997f
shuzhan <2651283633@qq.com>
2016-04-26 09:13:52 +0800

    右键菜单

--- app/main/szRightClick.js
+++ app/main/szRightClick.js
@@ -5,30 +5,18 @@ App.directive('szRightClick', ['$window', function ($window) {
     return {
         restrict: 'A',
         link: function ($scope, element, attrs) {
-            $scope.mainStateOpened = false;
-            $scope.guestOut=function(room){
-                $scope.$emit('popGuestOut',room);
-            };
-            $scope.takeDeposit=function(){
-                alert(room.roomId);
-            };
-            $scope.otherItem=function(){
-                alert(room.roomId);
-            };
+            var menuElement = angular.element(document.getElementById(attrs.szRightClick));
             element.bind('contextmenu', function (event) {
                 $scope.$apply(function () {
                     event.preventDefault();
-                    $scope.mainStateOpened=true;
-                    $scope.$emit('mainStateOpened',true);
+                    menuElement.css('display','block');
                 });
             });
             angular.element($window).bind('click', function (event) {
-                if ($scope.mainStateOpened) {
                     $scope.$apply(function () {
                         event.preventDefault();
-                        $scope.mainStateOpened=false;
+                        menuElement.css('display','none');
                     });
-                }
             });
         }
     };
