8f90f34802e86e6b56bcc99c2c3f70cc841372cb
liu dan <sichengde1@163.com>
2016-04-26 17:12:52 +0800

    团队开房修改teamCheckIn.html

--- app/main/szRightClick.js
+++ app/main/szRightClick.js
@@ -7,12 +7,17 @@ App.directive('szRightClick', ['$window', function ($window) {
         link: function ($scope, element, attrs) {
             var menuElement = angular.element(document.getElementById(attrs.szRightClick));
             element.bind('contextmenu', function (event) {
+                var oEvent=event;
                 $scope.$apply(function () {
                     event.preventDefault();
                     menuElement.css('display','block');
+                    menuElement.css('top','0');
                 });
+                menuElement.style.left=oEvent.clientX+'px';
+                menuElement.style.top=oEvent.clientY+'px';
             });
-            angular.element($window).bind('click', function (event) {
+
+                angular.element($window).bind('click', function (event) {
                     $scope.$apply(function () {
                         event.preventDefault();
                         menuElement.css('display','none');
